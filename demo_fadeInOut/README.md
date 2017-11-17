beeb7a0dcfde1b2681e374d4ba4a8927a230361a
special-wen <2142866269@qq.com>
2017-11-17 12:35:11 +0800

    README.md

---
+++ demo_fadeInOut/README.md
@@ -0,0 +1,43 @@
+#淡入淡出效果
+## js原生实现
+### js封装fadeIn(),fadeOut()函数的实现
+- fadeIn()
+> 将需要淡入的元素在css样式中添加```display:none```。之后进行js操作：通过```ele.style.display = 'block'```设置显示元素，并将该元素的透明度改为0;之后进行封装函数。
+```html
+    (function () {
+            ele.style.opacity = opa;
+            opa = opa +10;
+            if(opa <= opacity){
+                setTimeout(arguments.callee,speed);
+            }
+        })();
+```
+> 通过```arguments.callee```实现递归。使得透明度以10为基数增加。
+
+- fadeOut()
+
+> 在js中将需要淡出的元素的style的display更改属性为none.设置隐藏。并不断的将透明度改为0.原理和淡入一样。
+
+源码地址：https://github.com/special-wen/demo/tree/master/demo_fadeInOut/js%E5%8E%9F%E7%94%9F
+
+## css实现
+### 实现鼠标移入时淡入鼠标移除时淡出
+- transition
+#### 利用css3新增的属性transition实现。
+```css
+#demo li{
+    list-style: none;
+    font-size: 18px;
+    color: #8fa1c7;
+    transition: opacity 2s;
+    opacity: 0;
+}
+
+#demo li:hover{
+    opacity: 1;
+    transition-duration: 0s;
+}
+```
+> ```transition:opacity 2s```表示为改变属性opacity:0一共2s。当鼠标移入时，将透明度更改为1
+
+源码地址：https://github.com/special-wen/demo/tree/master/demo_fadeInOut/css%E5%AE%9E%E7%8E%B0
--- demo_fadeInOut/js原生/fadeInOut.js
+++ demo_fadeInOut/js原生/fadeInOut.js
@@ -1,59 +1,51 @@
-window.onload = function () {
-    function fadeIn(ele,speed,opacity){
-        //ele需要进行淡入的元素
-        //speed为淡入的速度
-        //opacity淡入到指定的透明度
-        speed = speed || 20;
-        opacity = opacity || 100;
-        //显示元素
-        ele.style.display = 'block';
-        //将透明度该为0
-        ele.style.opacity = '0/100';
+function fadeIn(ele,speed,opacity){
+    //ele需要进行淡入的元素
+    //speed为淡入的速度
+    //opacity淡入到指定的透明度
+    speed = speed || 20;
+    opacity = opacity || 100;
+    //显示元素
+    ele.style.display = 'block';
+    //将透明度该为0
+    ele.style.opacity = '0/100';
+    //初始化透明度为0
+    let opa = 0;
+    //循环将透明度增加，以10增加。
+    (function () {
+        ele.style.opacity = opa;
+        opa = opa +10;
+        if(opa <= opacity){
+            setTimeout(arguments.callee,speed);
+        }
+    })();
+}
+//淡出
+function fadeOut(ele,speed,opacity) {
+    speed = speed || 20;
+    opacity = opacity || 0;
+    let opa = 100;
+    (function () {
+        ele.style.opacity = opa;
+        opa = opa-5;
+        if(opa >= opacity){
+            setTimeout(arguments.callee,speed);
+            console.log(opa);
+        }else if(opa < 0){
+            ele.style.display = 'none';
+        }
+    })();
+}
+document.getElementById("In").onclick = function () {
+    let needFadeIn = document.getElementById("fadeIn");
+    fadeIn(needFadeIn);
+}
 
-        //初始化透明度为0
-        let opa = 0;
-        //循环将透明度增加，以10增加。
-        (function () {
-            ele.style.opacity = opa;
-            opa = opa +10;
-            if(opa <= opacity){
-                setTimeout(arguments.callee,speed);
-            }
-        })();
-    }
-
-    //淡出
-    function fadeOut(ele,speed,opacity) {
-        speed = speed || 20;
-        opacity = opacity || 0;
-        let opa = 100;
-        (function () {
-
-            ele.style.opacity = opa;
-            opa = opa-5;
-            if(opa >= opacity){
-                setTimeout(arguments.callee,speed);
-                console.log(opa);
-            }else if(opa < 0){
-                ele.style.display = 'none';
-            }
-        })();
-    }
-
-    document.getElementById("In").onclick = function () {
-        let needFadeIn = document.getElementById("fadeIn");
-        fadeIn(needFadeIn,3000);
-    }
-
-    document.getElementById("Out").onclick = function () {
-        let needFadeOut = document.getElementById("fadeOut");
-        alert('fadeOut');
-        fadeOut(needFadeOut);
-    }
-
-    document.getElementById("To").onclick = function () {
-        let needFadeTo = document.getElementById("fadeTo");
-        fadeOut(needFadeTo,30,10);
-    }
-
+document.getElementById("Out").onclick = function () {
+    let needFadeOut = document.getElementById("fadeOut");
+    alert('fadeOut');
+    fadeOut(needFadeOut);
+}
+document.getElementById("To").onclick = function () {
+    let needFadeTo = document.getElementById("fadeTo");
+    fadeOut(needFadeTo,30,10);
 }
